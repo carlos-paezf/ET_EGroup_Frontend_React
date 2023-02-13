f1103100859c2c685cb97da8e49994ffc63cd63c
carlos-paezf <cpaezferreira@gmail.com>
2023-02-12 19:51:18 -0500

    Add Header Component

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,40 @@
+import { Box, Divider, Grid, Typography } from '@mui/material'
+import { FC, ReactNode } from "react"
+
+
+type HeaderProps = {
+    title: string
+    description: string
+    element?: ReactNode
+}
+
+
+export const HeaderComponent: FC<HeaderProps> = ( { title, description, element } ) => {
+    return (
+        <>
+            <Box sx={ { width: "100%", height: "auto", mt: 7, mb: 3 } }>
+                <Grid container direction="row"
+                    justifyContent="center" alignItems="center" sx={ { height: "100%", width: "100%" } }>
+                    <Grid item xs={ 12 }>
+                        <Grid container direction="column"
+                            justifyContent="center" alignItems="center" sx={ { height: "100%", width: "100%" } }>
+                            <Grid item>
+                                <Typography variant='h3' align='center'>{ title }</Typography>
+                            </Grid>
+
+                            <Grid item sx={ { mt: 2 } }>
+                                <Typography align='center'>{ description }</Typography>
+                            </Grid>
+
+                            {
+                                !!element && <Grid item sx={ { mt: 4, width: "100%" } }>{ element }</Grid>
+                            }
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Box>
+
+            <Divider />
+        </>
+    )
+}
