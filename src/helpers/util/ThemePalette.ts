d78c084f07ec00d9468432303993878acf03f948
carlos-paezf <cpaezferreira@gmail.com>
2023-02-13 19:52:35 -0500

    Update Theme Palete

--- src/helpers/util/ThemePalette.ts
+++ src/helpers/util/ThemePalette.ts
@@ -1,8 +1,9 @@
 export enum ThemePalette {
     BG = "#F5F5FA",
     BG_GLASS = "rgba(245, 245, 250, .5)",
-    PRIMARY = "#3F0055",
-    LIGHT_PRIMARY = "rgba(155, 0, 212, 0.5)",
+    PRIMARY = "#0C5500",
+    LIGHT_PRIMARY = "rgba(17, 128, 0, 0.863)",
+    SECONDARY = '#B1BFCA',
     FONT_GLOBAL = "'Libre Franklin', monospace",
     ERROR_MAIN = "#F44336",
     BG_ERROR_MAIN = "RGBA(224, 67, 54, .5)",
