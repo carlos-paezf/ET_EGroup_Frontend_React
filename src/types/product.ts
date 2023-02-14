9d2eea23bf2f271682c142486b441db6098e4fd1
carlos-paezf <cpaezferreira@gmail.com>
2023-02-13 20:04:17 -0500

    Add Product Type

---
+++ src/types/product.ts
@@ -0,0 +1,11 @@
+export type ProductType = {
+    id: number
+    description: string
+    stock: number
+    price: number
+    slug: string
+    tags: string[]
+    title: string
+    rating: number
+    image?: string
+}
