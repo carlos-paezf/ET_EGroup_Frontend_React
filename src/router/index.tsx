39c7be21ba93ea225a7ab749abec8ae1a2323228
carlos-paezf <cpaezferreira@gmail.com>
2023-02-14 20:27:45 -0500

    Update router links

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,26 +1,40 @@
 import { createBrowserRouter, Navigate } from 'react-router-dom'
 import { App } from '../App'
+import { CatalogList } from '../components/Products/Catalog'
+import { IndividualProduct } from '../components/Products/IndividualProduct'
+import { CompanyPage } from '../pages/Company'
 import { HomePage } from '../pages/Home'
 import { NotFoundPage } from '../pages/NotFound'
+import { ProductCatalogPage } from '../pages/ProductCatalog'
 
 
 export const router = createBrowserRouter(
     [
         {
-            path: 'home',
+            path: '',
             element: <App />,
             children: [
                 { path: '', element: <HomePage /> },
-                { path: '*', element: <NotFoundPage /> }
+                {
+                    path: 'products',
+                    element: <ProductCatalogPage />,
+                    children: [
+                        { path: '', element: <CatalogList /> },
+                        { path: ':slug', element: <IndividualProduct /> },
+                        { path: '*', element: <Navigate to="list" /> }
+                    ]
+                },
+                { path: 'company', element: <CompanyPage /> },
+                { path: '*', element: <NotFoundPage /> },
             ]
         },
         {
             path: '/',
-            element: <Navigate to="home" />
+            element: <Navigate to="" />
         },
         {
             path: '*',
-            element: <Navigate to="home" />
+            element: <Navigate to="" />
         }
     ]
 )
